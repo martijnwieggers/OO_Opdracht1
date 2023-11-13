78d940a458763605df88259c047561efefbe9de0
Martijn Wieggers <127099600+martijnwieggers@users.noreply.github.com>
2023-11-13 16:53:31 +0100

    instructies toegevoegd

--- Opdracht1/Persoon.cs
+++ Opdracht1/Persoon.cs
@@ -9,12 +9,27 @@ namespace Opdracht1
 {
     public class Persoon
     {
-        private string _naam;
-
-        public string Naam
+        //default constructor
+        public Persoon()
         {
-            get { return _naam; }
-            set { _naam = value; }
+        
         }
+
+
+        private string _voornaam ="";
+        //Taak 5: voeg een prive variable toe voor de achternaam begin prive variable met _
+
+        public string Voornaam
+        {
+            get { return _voornaam; }
+            set { _voornaam = value; }
+        }
+
+        //Taak 6: voeg een publieke variable toe voor Achternaam
+        //        deze bewerkt in de get en set de nieuwe prive variabele voor achternaam
+        //        deze prive variabele heeft de naar Achternaam en werkt zoals Voornaam
+
+        //Taak 7: Ga terug naar Program.cs
+
     }
 }
--- Opdracht1/Program.cs
+++ Opdracht1/Program.cs
@@ -6,17 +6,25 @@ using Opdracht1;
 List<Persoon> Personen = new List<Persoon>();
 
 Persoon eerste = new Persoon();
-eerste.Naam = "Jan";
+eerste.Voornaam = "Jan";
 Personen.Add(eerste);
 
-//Taak 1: Maak nog een Persoon object aan met de variabelenaam tweede
+//Taak 1: Maak nog een Persoon object aan met de variabelenaam tweede (tip: regel 8 lijkt er heel erg op)
+
 //Taak 2: Zet de naam van het Persoon object met de variabelenaam op "Henk"
+
 //Taak 3: Voeg dit object toe aan de Lijst met de naam Personen
 
 
 
 foreach(var eenpersoon in Personen)
 {
-    Console.WriteLine(eenpersoon.Naam);
+    Console.WriteLine(eenpersoon.Voornaam);
 }
+
+//Taak 4: Ga naar persoon.cs en voor daar de taken uit om Achternaam toe te voegen
+
+//Taak 8: Stel de achternaam van ieder Persoon object dat je aanmaakt in op "Jansen"
+
+//Taak 9: Pas binnen de foreach aan, zodat Voornaam en Achternaam achter elkaar worden afgedrukt.
 
