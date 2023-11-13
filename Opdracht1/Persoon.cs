26cbd5ca920bb0f1eb128108171445dc5041bedf
Martijn Wieggers <127099600+martijnwieggers@users.noreply.github.com>
2023-11-13 16:30:33 +0100

    initial commit

---
+++ Opdracht1/Persoon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdracht1
+{
+    public class Persoon
+    {
+        private string _naam;
+
+        public string Naam
+        {
+            get { return _naam; }
+            set { _naam = value; }
+        }
+    }
+}
--- Opdracht1/Program.cs
+++ Opdracht1/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using Opdracht1;
+
+List<Persoon> Personen = new List<Persoon>();
+
+Persoon eerste = new Persoon();
+eerste.Naam = "Jan";
+Personen.Add(eerste);
+
+//Taak 1: Maak nog een Persoon object aan met de variabelenaam tweede
+//Taak 2: Zet de naam van het Persoon object met de variabelenaam op "Henk"
+//Taak 3: Voeg dit object toe aan de Lijst met de naam Personen
+
+
+
+foreach(var eenpersoon in Personen)
+{
+    Console.WriteLine(eenpersoon.Naam);
+}
+
